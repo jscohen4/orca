57eff25dfa489ff50086f6666e78f3df66505d8e
JC <joncohen@ucdavis.edu>
2018-08-14 13:45:28 -0700

    works up to 50 year windows

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from datetime import datetime
 now = datetime.now().strftime('Last modified %Y-%m-%d %H:%M:%S')
 #Each of these booleans determines the actions that will be run by the model 
 
-projection = False #True if running a single climate projection
+projection = True #True if running a single climate projection
 calc_R2s = True #True if calculating R2s (only relevant for historical scenario)
 plot = False #True if plotting outputs, need calc_R2s to also be true if plotting historical results!!!!
 
@@ -17,7 +17,7 @@ ORO_shift = 0
 FOL_shift = 0
 index_exceedence_sac = 8
 window_type = 'historical'
-window_length = 30
+window_length = 50
 
 process_hist_data = False#True if changing any historical data inputs, or downloading updated data from cdec
 ###Only relevant if processing historical data
@@ -26,7 +26,7 @@ hist_indices = True #True if running calc_indices scriptwater_day
 hist_forcast = True #True if running updated forecast
 
 sc = 'access1-0_rcp45_r1i1p1' #cmip5 climate scenario to use, if projection = True
-process_climate_data = False #only mark True if running climate projection and/or processing projection input data
+process_climate_data = True #only mark True if running climate projection and/or processing projection input data
 ####### only relevant if processing projection data
 climate_indices = False
 climate_forecasts = True
