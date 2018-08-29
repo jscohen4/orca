fc37164c1880bbc523a065f4e0127b96053557ac
JC <joncohen@ucdavis.edu>
2018-08-29 09:06:46 -0700

    fix inflow forecasts

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from datetime import datetime
 now = datetime.now().strftime('Last modified %Y-%m-%d %H:%M:%S')
 #Each of these booleans determines the actions that will be run by the model 
 
-projection = True #True if running a single climate projection
-calc_R2s = False #True if calculating R2s (only relevant for historical scenario)
-plot = False #True if plotting outputs, need calc_R2s to also be true if plotting historical results!!!!
+projection = False #True if running a single climate projection
+calc_R2s = True #True if calculating R2s (only relevant for historical scenario)
+plot = True #True if plotting outputs, need calc_R2s to also be true if plotting historical results!!!!
 change_inflow_exeedance = False
 
 #######Define a few parameters
@@ -44,7 +44,7 @@ hist_indices = True #True if running calc_indices scriptwater_day
 hist_forcast = True #True if running updated forecast
 
 sc = 'access1-0_rcp45_r1i1p1' #cmip5 climate scenario to use, if projection = True
-process_climate_data = True #only mark True if running climate projection and/or processing projection input data
+process_climate_data = False #only mark True if running climate projection and/or processing projection input data
 ####### only relevant if processing projection data
 climate_indices = False
 climate_forecasts = True
--- orca/reservoir.py
+++ orca/reservoir.py
@@ -158,10 +158,10 @@ class Reservoir():
     ##each timestep before the reservoirs' individual step function is called
     #also used to obtain inflow forecasts
     # self.exceedence_level = -1*min((self.WYI[t-1] - 10.0)*0.8,-2)##how conservative are they being about the flow forecasts (ie, 90% exceedance level, 75% exceedance level, etc)
-    self.forecast[t] = max(0,self.slope[t] * self.obs_snow[t] + self.intercept[t] + self.std[t]*z_table_transform[self.exceedance[self.wyt[t]]])# * 1000 #based on forecast regression
+    self.forecast[t] = max(0,self.slope[t] * self.obs_snow[t] + self.intercept[t] + self.std[t]*z_table_transform[self.exceedance[self.wyt[t]]])/2# * 1000 #based on forecast regression
     if dowy == 0:
       self.calc_expected_min_release(t)##what do they expect to need to release for env. requirements through the end of september
-      self.forecast[t] = max(0,self.slope[t+1] * self.obs_snow[t+1] + self.intercept[t+1]+ self.std[t]*z_table_transform[self.exceedance[self.wyt[t]]]) #* 1000 #based on forecast regression
+      self.forecast[t] = max(0,self.slope[t+1] * self.obs_snow[t+1] + self.intercept[t+1]+ self.std[t]*z_table_transform[self.exceedance[self.wyt[t]]])/2 #* 1000 #based on forecast regression
     # if d == 0:
       # self.forecast[t] = max(0,self.slope[t] * self.obs_snow[t] + self.intercept[t]) * 1000 #based on forecast regression
     # self.available_storage[t] = max(0,self.S[t-1] - self.carryover_target[self.wyt[t]] + self.forecast[t] - self.cum_min_release[dowy])
