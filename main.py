42e491cd020d8aa720b72b44fbcb8b51efae6d9c
JC <joncohen@ucdavis.edu>
2018-12-05 08:26:32 -0800

    second scenario added

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ ORO_shift = 0
 FOL_shift = 0
 index_exceedance_sac = 8
 window_type = 'rolling'
-window_length = 20
+window_length = 50
 SHA_exceedance = {"W": 2, "AN": 2, "BN": 2, "D": 2, "C": 2}
 ORO_exceedance = {"W": 2, "AN": 2, "BN": 2, "D": 2, "C": 2}
 FOL_exceedance = {"W": 10, "AN": 10, "BN": 5, "D": 2, "C": 1}
@@ -38,14 +38,14 @@ ORO_exceedance_hist = {"W": 2, "AN": 2, "BN": 2, "D": 2, "C": 2}
 FOL_exceedance_hist = {"W": 10, "AN": 10, "BN": 5, "D": 2, "C": 1}
 
 
-process_hist_data = True#True if changing any historical data inputs, or downloading updated data from cdec
+process_hist_data = False#True if changing any historical data inputs, or downloading updated data from cdec
 ###Only relevant if processing historical data
 cdec = False # True if downloading up-to-date cdec data
 hist_indices = True #True if running calc_indices scriptwater_day
 hist_forcast = True #True if running updated forecast
 
-sc = 'access1-0_rcp45_r1i1p1' #cmip5 climate scenario to use, if projection = True
-process_climate_data = False #only mark True if running climate projection and/or processing projection input data
+sc = 'access1-0_rcp85_r1i1p1' #cmip5 climate scenario to use, if projection = True
+process_climate_data = True #only mark True if running climate projection and/or processing projection input data
 ####### only relevant if processing projection data
 climate_indices = True
 climate_forecasts = True
