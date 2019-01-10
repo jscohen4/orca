0f763b9cc630321f3c78c5822acc6336ddfcf512
JC <joncohen@ucdavis.edu>
2019-01-10 12:35:31 -0800

    fix run all projections file

--- run_all_climate_projections.py
+++ run_all_climate_projections.py
@@ -11,6 +11,18 @@ run_projection = True
 consolidate_outputs = True
 consolidate_inputs = False
 #need climate data folders for this, which are too large for github (a few are present in repository for example)
+#######Define a few parameters
+SHA_shift = 0
+ORO_shift = 0
+FOL_shift = 0
+index_exceedance_sac = 8
+window_type = 'rolling'
+window_length = 30
+SHA_exceedance = {"W": 2, "AN": 2, "BN": 2, "D": 2, "C": 2}
+ORO_exceedance = {"W": 2, "AN": 2, "BN": 2, "D": 2, "C": 2}
+FOL_exceedance = {"W": 10, "AN": 10, "BN": 5, "D": 2, "C": 1}
+
+
 with open('orca/data/scenario_names.txt') as f:
 	scenarios = f.read().splitlines()
 i = 0
@@ -20,24 +32,27 @@ for sc in scenarios:
 	call(['mkdir', 'orca/data/scenario_runs/%s'%sc])
 	if climate_indices:
 		input_df = pd.read_csv('orca/data/input_climate_files/%s_input_data.csv'%sc, index_col = 0, parse_dates = True)
-		proj_ind_df = process_projection(input_df,'orca/data/json_files/gains_regression.json') 
-		proj_ind_df.to_csv('orca/data/scenario_runs/%s/orca-data-processed-%s.csv'%(sc,sc))
+		gains_loop_df = pd.read_csv('orca/data/historical_runs_data/gains_loops.csv', index_col = 0, parse_dates = True)
+		OMR_loop_df = pd.read_csv('orca/data/historical_runs_data/OMR_loops.csv', index_col = 0, parse_dates = True)
+		proj_ind_df = process_projection(input_df,gains_loop_df,OMR_loop_df,'orca/data/json_files/gains_regression.json','orca/data/json_files/inf_regression.json',window = window_type)  
+		proj_ind_df.to_csv('orca/data/individual_projection_runs/%s/orca-data-processed-%s.csv'%(sc,sc))
 	if climate_forecasts:
 		if not climate_indices:
-			proj_ind_df = pd.read_csv('orca/data/scenario_runs/%s/orca-data-processed-%s.csv'%(sc,sc),index_col = 0, parse_dates = True)
+			proj_ind_df = pd.read_csv('orca/data/individual_projection_runs/%s/orca-data-processed-%s.csv'%(sc,sc), index_col = 0, parse_dates = True)
 		WYI_stats_file = pd.read_csv('orca/data/forecast_regressions/WYI_forcasting_regression_stats.csv', index_col = 0, parse_dates = True)
 		carryover_stats_file = pd.read_csv('orca/data/forecast_regressions/carryover_regression_statistics.csv', index_col = 0, parse_dates = True)
-		forc_df= projection_forecast(proj_ind_df,WYI_stats_file,carryover_stats_file)
-		forc_df.to_csv('orca/data/scenario_runs/%s/orca-data-climate-forecasted-%s.csv'%(sc,sc))
+		forc_df= projection_forecast(proj_ind_df,WYI_stats_file,carryover_stats_file,window_type,window_length, index_exceedance_sac)
+		forc_df.to_csv('orca/data/individual_projection_runs/%s/orca-data-climate-forecasted-%s.csv'%(sc,sc))
 	if run_projection: 
-		model = Model('orca/data/scenario_runs/%s/orca-data-climate-forecasted-%s.csv'%(sc,sc), 'orca/data/historical_runs_data/results.csv',sd='10-01-1999',projection = True, sim_gains = True) #climate scenario test
-		projection_results = model.simulate() # takes a while... save results
-		projection_results.to_csv('orca/data/scenario_runs/%s/%s-results.csv'%(sc,sc))
+		model = Model('orca/data/individual_projection_runs/%s/orca-data-climate-forecasted-%s.csv'%(sc,sc), 'orca/data/historical_runs_data/results.csv',SHA_shift, ORO_shift, FOL_shift,sd='10-01-1999',projection = True, sim_gains = True) #climate scenario test
+		results = model.simulate() # takes a while... save results
+		results.to_csv('orca/data/individual_projection_runs/%s/%s-results.csv'%(sc,sc))
 
 if consolidate_outputs: 
-	result_ids = ['SHA_storage','SHA_out','SHA_target','SHA_out_to_delta','SHA_tocs','FOL_storage','FOL_out',
-								'FOL_target','FOL_out_to_delta','FOL_tocs','ORO_storage','ORO_out','ORO_target','ORO_out_to_delta',
-								'ORO_tocs','DEL_in','DEL_out','DEL_TRP_pump','DEL_HRO_pump']
+	result_ids =['SHA_storage','SHA_out','SHA_target','SHA_out_to_delta','SHA_tocs','FOL_storage','FOL_out',
+				      'FOL_target','FOL_out_to_delta','FOL_tocs','ORO_storage','ORO_out','ORO_target','ORO_out_to_delta',
+				      'ORO_tocs','DEL_in','DEL_out','DEL_TRP_pump','DEL_HRO_pump','SHA_sodd','SHA_spill', 
+					'ORO_sodd','ORO_spill','FOL_sodd','FOL_spill', 'DEL_X2','ORO_forecast','FOL_forecast','SHA_forecast','DEL_SODD_CVP','DEL_SODD_SWP']
 	for obj in result_ids:
 		df = pd.DataFrame()
 		print(obj)
