f68c09f023264d847a9256f284734a20ac1aed2d
JC <joncohen@ucdavis.edu>
2020-05-13 15:49:42 -0700

    add adaptation scripts

--- job-slurm-cc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-#SBATCH -n 97            # Total number of processors to request
-#SBATCH -p high           # Queue name hi/med/lo
-#SBATCH -t 70:00:00        # Run time (hh:mm:ss) - 24 hours
-#SBATCH --mail-user= <your email>            # address for email notification
-#SBATCH --mail-type=ALL                  # email at Begin and End of job
-
-export PATH=<your path>:$PATH
-mpirun -n 97 python main-parallel-cc.py
